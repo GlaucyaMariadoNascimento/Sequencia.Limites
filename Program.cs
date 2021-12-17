e5488285b3bdc5c352a91db7bc111f2c58d95200
Glaucya <stobbeglaucya@gmail.com>
2021-12-17 15:12:39 -0300

    Resolução exercício sequencia limites

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sequencia.Limites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Início: ");
+            int inicio = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Fim: ");
+            int fim = Convert.ToInt32(Console.ReadLine());
+
+            if (fim >= inicio){
+
+                int n = inicio;
+
+                while (n <= fim){
+                    Console.Write($"{n} ");
+                    n += 1;
+                }
+
+            }
+            else {
+                Console.WriteLine("Final post");
+
+            }
+            
+            
+        }
+    }
+}
